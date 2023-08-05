6a6e989529ad9c307fa0580fcb0e5bd8709c3613
Vladimir Byelov <vladimir.byeloff@gmail.com>
2023-08-05 17:01:31 +0300

    test_arrs changed again

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,11 +1,11 @@
-from utils.arrs import my_slice, get
+from utils import arrs
 
 
 def test_get():
-    assert get([1, 2, 3], 1, "test") == 2
-    assert get([], -1, "test") == "test"
+    assert arrs.get([1, 2, 3], 1, "test") == 2
+    assert arrs.get([], -1, "test") == "test"
 
 
 def test_slice():
-    assert my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
+    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
